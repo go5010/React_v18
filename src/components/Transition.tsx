1e3e053b36328a7d855005872df0b4b2c560cce3
go5010 <gota.4305@gmail.com>
2024-01-23 20:16:02 +0900

    part15完了

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+
 type Task = {
   id: number;
   title: string;
@@ -11,7 +14,7 @@ const member = {
 };
 
 const generateDummyTasks = (): Task[] => {
-  return Array(10000)
+  return Array(1000)
     .fill("")
     .map((_, index) => {
       const addedIndex = index + 1;
@@ -29,12 +32,55 @@ const generateDummyTasks = (): Task[] => {
 };
 
 const tasks = generateDummyTasks();
-console.log(tasks);
+
+const filteringAssignee = (assignee: string) => {
+  if (assignee === "") return tasks;
+  return tasks.filter((task) => task.assignee === assignee);
+};
 
 export const Transition = () => {
+  const [selectedAssignee, setSelectedAssignee] = useState<string>("");
+  const [taskList, setTaskList] = useState<Task[]>(tasks);
+
+  const onClickAssignee = (assignee: string) => {
+    setSelectedAssignee(assignee);
+    setTaskList(filteringAssignee(assignee));
+  };
+
   return (
     <div>
       <p>transition</p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Avatar
+          isSelected={selectedAssignee === member.a}
+          onClick={onClickAssignee}
+        >
+          {member.a}
+        </Avatar>
+        <Avatar
+          isSelected={selectedAssignee === member.b}
+          onClick={onClickAssignee}
+        >
+          {member.b}
+        </Avatar>
+        <Avatar
+          isSelected={selectedAssignee === member.c}
+          onClick={onClickAssignee}
+        >
+          {member.c}
+        </Avatar>
+      </div>
+      <br />
+      <button onClick={() => onClickAssignee("")}>リセット</button>
+      {taskList.map((task) => (
+        <div
+          key={task.id}
+          style={{ width: "300px", margin: "auto", background: "lavender" }}
+        >
+          <p>タイトル：{task.title}</p>
+          <p>担当：{task.assignee}</p>
+        </div>
+      ))}
     </div>
   );
 };
